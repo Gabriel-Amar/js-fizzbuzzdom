381dbc682ccedbf5749ebbefecf59cb805076167
Gabriel-Amar <amariutei.gabriel1@gmail.com>
2022-03-29 10:55:29 +0200

    css e js: aggiunta stile

--- js/script.js
+++ js/script.js
@@ -1,35 +1,34 @@
-let numeri = 100;
 
-for (let i = 1; i <= numeri; i++){
+let numeri = 100;
+const serie = document.getElementById("serie");
+const row = document.createElement("div");
+row.setAttribute("class", "row g-3")    
+    for (let i = 1; i <= 100; i++){
+    const col = document.createElement("div")
+    col.setAttribute("class", "col-12 col-sm-6 col-md-4 col-lg-2");
+    const square = document.createElement("div");
+    square.setAttribute("class", "square");
+    
     if( i % 3 === 0 && i % 5 === 0) {
-        const mioDiv = document.createElement("div");
-        mioDiv.innerHTML = i + " fizzbuzz";
-        serie.append(mioDiv)
-        mioDiv.classList.add("red")
+        square.innerHTML = i + " fizzbuzz";
+        square.classList.add("red")
     } else {
         if( i % 3 === 0 ) {
-            const mioDiv = document.createElement("div");
-            mioDiv.innerHTML =  i + " Fizz";
-            serie.append(mioDiv)
-            mioDiv.classList.add("blue")
+            square.innerHTML =  i + " Fizz";
+            square.classList.add("blue")
         }
         if( i % 5 === 0 ) {
-            const mioDiv = document.createElement("div");
-            mioDiv.innerHTML =  i + " Buzz";
-            serie.append(mioDiv)
-            mioDiv.classList.add("green")
+            square.innerHTML =  i + " Buzz";
+            square.classList.add("green")
         }
         else{
-            const mioDiv = document.createElement("div");
-            mioDiv.innerHTML =  i;
-            serie.append(mioDiv)
+            square.innerHTML =  i;
         }
     }
+    col.append(square)
+    row.append(col)
 }
-
-
-
-
+serie.append(row)
 
 
 
